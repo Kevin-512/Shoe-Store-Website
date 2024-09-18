78fc35cf3772c7e0f3c00c93fe1a1bf87cb2d3ff
Kevin-512 <108360149+Kevin-512@users.noreply.github.com>
2024-09-18 14:58:31 +0100

    Navbar completed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,38 @@
-
+import { GiRunningShoe } from "react-icons/gi";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <div className="flex flex-row gap-2 items-center">
+            <GiRunningShoe className="text-4xl text-lime-600" />
+            <h2 className="font-montserrat text-4xl font-extrabold bg-gradient-to-r from-green-700 to-black bg-clip-text text-transparent">
+              SneakerZ
+            </h2>
+          </div>
+        </a>
+        <ul className="flex flex-1 justify-center items-center gap-16 px-8 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
 
-export default Nav
+        <div className="hidden max-lg:block">
+          <RxHamburgerMenu className="text-4xl" />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
